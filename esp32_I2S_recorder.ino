afcc4bf660bf30717dd7e34c535ba3cd40cf7a64
1806Henry <44114631+1806Henry@users.noreply.github.com>
2021-04-12 17:56:57 +0200

    Update esp32_I2S_recorder.ino

--- esp32_I2S_recorder.ino
+++ esp32_I2S_recorder.ino
@@ -56,6 +56,24 @@ void setup() {
   }
   
   ac.SetLineSource();
+   ac.SetI2sWordSize(AC101::WORD_SIZE_16_BITS);
+ac.SetI2sSampleRate(AC101::SAMPLE_RATE_44100);
+ac.SetI2sClock(AC101::BCLK_DIV_16, false, AC101::LRCK_DIV_32, false);
+ac.SetI2sMode(AC101::MODE_SLAVE);
+ac.SetI2sWordSize(AC101::WORD_SIZE_16_BITS);
+ac.SetI2sFormat(AC101::DATA_FORMAT_I2S);
+
+/* nur bei activen output */
+ac.SetVolumeSpeaker(3);
+ac.SetVolumeHeadphone(99);
+
+/* funktioniert auch mit aktivem output */
+ac.SetLineSource();
+
+// Enable amplifier
+pinMode(GPIO_PA_EN, OUTPUT);
+digitalWrite(GPIO_PA_EN, HIGH);
+
   
   delay(4000);
    Serial.println("start");
