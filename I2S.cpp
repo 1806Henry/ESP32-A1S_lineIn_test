a0d074f748b947d5f6d37cc6feb9c63632d4a301
1806Henry <44114631+1806Henry@users.noreply.github.com>
2021-04-14 00:24:16 +0200

    Update I2S.cpp

--- I2S.cpp
+++ I2S.cpp
@@ -5,7 +5,7 @@ void I2S_Init()
   i2s_config_t i2s_config = {
       .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
       .sample_rate = 44100,
-      .bits_per_sample = i2s_bits_per_sample_t(32),
+      .bits_per_sample = i2s_bits_per_sample_t(16),
       .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
       .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
       .intr_alloc_flags = 0,
