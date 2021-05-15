dac45c43ada4dbae065817ca2d18d43e975835cb
1806Henry <44114631+1806Henry@users.noreply.github.com>
2021-05-15 11:08:08 +0200

    Update esp32_I2S_recorder.ino

--- esp32_I2S_recorder.ino
+++ esp32_I2S_recorder.ino
@@ -1,11 +1,10 @@
 #include "Arduino.h"
-#include <FS.h>
-#include "Wav.h"
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
 #include "AC101.h"
-#include "I2S.h"
+#include "driver/i2s.h"
+#include "esp_system.h"
 
 #define SD_CS         13
 #define SPI_MOSI      15
@@ -30,7 +29,7 @@
 static AC101 ac;
 
 const int record_time = 10;  // second
-const char filename[] = "/ttttt.wav";
+const char filename[] = "/test.wav";
 
 const int headerSize = 44;
 const int waveDataSize = record_time * 88000;
@@ -69,7 +68,7 @@ void setup() {
    ac.SetVolumeHeadphone(99);
 
    /* funktioniert auch mit aktivem output */
-   ac.SetLineSource();
+   ac.SetMicSource();
 
 // Enable amplifier
 pinMode(GPIO_PA_EN, OUTPUT);
@@ -77,13 +76,13 @@ digitalWrite(GPIO_PA_EN, HIGH);
 
   
   delay(4000);
-   Serial.println("start");
+  Serial.println("start");
   CreateWavHeader(header, waveDataSize);
   SD.remove(filename);
   file = SD.open(filename, FILE_WRITE);
   if (!file) return;
   file.write(header, headerSize);
-    pinMode(35, INPUT);
+  pinMode(35, INPUT);
   pinMode(27, OUTPUT);
   pinMode(26, OUTPUT);
   I2S_Init();
@@ -98,6 +97,86 @@ digitalWrite(GPIO_PA_EN, HIGH);
   }
   file.close();
   Serial.println("finish");
+}
+
+int I2S_Read(char *data, int numData){
+  return i2s_read_bytes(I2S_NUM_0, (char *)data, numData, portMAX_DELAY);
+}
+
+void I2S_Write(char *data, int numData){
+  i2s_write_bytes(I2S_NUM_0, (const char *)data, numData, portMAX_DELAY);
+}
+
+void I2S_Init(){
+  i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+      .sample_rate = 44100,
+      .bits_per_sample = i2s_bits_per_sample_t(16),
+      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+      .intr_alloc_flags = 0,
+      .dma_buf_count = 8,
+      .dma_buf_len = 64,
+      .use_apll = false
+    };
+  
+    i2s_pin_config_t pin_config;
+    pin_config.bck_io_num = 27;
+    pin_config.ws_io_num = 26;
+    pin_config.data_out_num = -1;
+    pin_config.data_in_num = 35;
+
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM_0, &pin_config);
+    i2s_set_clk(I2S_NUM_0, 44100, i2s_bits_per_sample_t(16), I2S_CHANNEL_STEREO);
+}
+
+void CreateWavHeader(byte* header, int waveDataSize){
+  header[0] = 'R';
+  header[1] = 'I';
+  header[2] = 'F';
+  header[3] = 'F';
+  unsigned int fileSizeMinus8 = waveDataSize + 44 - 8;
+  header[4] = (byte)(fileSizeMinus8 & 0xFF);
+  header[5] = (byte)((fileSizeMinus8 >> 8) & 0xFF);
+  header[6] = (byte)((fileSizeMinus8 >> 16) & 0xFF);
+  header[7] = (byte)((fileSizeMinus8 >> 24) & 0xFF);
+  header[8] = 'W';
+  header[9] = 'A';
+  header[10] = 'V';
+  header[11] = 'E';
+  header[12] = 'f';
+  header[13] = 'm';
+  header[14] = 't';
+  header[15] = ' ';
+  header[16] = 0x10;  // linear PCM
+  header[17] = 0x00;
+  header[18] = 0x00;
+  header[19] = 0x00;
+  header[20] = 0x01;  // linear PCM
+  header[21] = 0x00;
+  header[22] = 0x01;  // monoral
+  header[23] = 0x00;
+  header[24] = 0x44;  // sampling rate 44100
+  header[25] = 0xAC;
+  header[26] = 0x00;
+  header[27] = 0x00;
+  header[28] = 0x88;  // Byte/sec = 44100x2x1 = 88200
+  header[29] = 0x58;
+  header[30] = 0x01;
+  header[31] = 0x00;
+  header[32] = 0x02;  // 16bit monoral
+  header[33] = 0x00;
+  header[34] = 0x10;  // 16bit
+  header[35] = 0x00;
+  header[36] = 'd';
+  header[37] = 'a';
+  header[38] = 't';
+  header[39] = 'a';
+  header[40] = (byte)(waveDataSize & 0xFF);
+  header[41] = (byte)((waveDataSize >> 8) & 0xFF);
+  header[42] = (byte)((waveDataSize >> 16) & 0xFF);
+  header[43] = (byte)((waveDataSize >> 24) & 0xFF);
 }
 
 void loop() {
